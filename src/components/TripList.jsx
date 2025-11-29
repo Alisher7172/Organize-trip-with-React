da502c304ea5ef04153b8ce93c640eb239c46991
Your Name <alisher20021212@gmail.com>
2025-11-29 21:45:21 +0200

    fix bug

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -22,24 +22,50 @@ function TripList() {
    
 
   return (
-    <div className='trip-list'>
-        <h1 className='main-txt animate__animated animate__flash'><i>Trip List</i></h1>
-        {isPending && <div className='loading animate__animated animate__heartBeat'><i>Loading Trips...</i></div>}
-        {error && <div className='error animate__animated animate__shakeX'><i>{error}</i></div>}
-        <ul>
-            {trips && Array.isArray(trips) && trips.map(trip => (
-                <li key={trip.id} className='animate__animated animate__bounce'>
-                    <h2><i>{trip.title}</i></h2>
-                    <p><i>{trip.price}</i></p>
-                </li>
-            ))}
-        </ul>
-        <div className='filters'>
-            <button className='animate__animated animate__rubberBand' onClick={() => setUrl('http://localhost:3000/trips')}>All Trips</button>
-            <button className='animate__animated animate__rubberBand' onClick={() => setUrl('http://localhost:3000/trips?location=USA')}>America Trips</button>
+    <div className="trip-list">
+      <h1 className="main-txt animate__animated animate__flash">
+        <i>Trip List</i>
+      </h1>
+      {isPending && (
+        <div className="loading animate__animated animate__heartBeat">
+          <i>Loading Trips...</i>
         </div>
+      )}
+      {error && (
+        <div className="error animate__animated animate__shakeX">
+          <i>{error}</i>
+        </div>
+      )}
+      {trips && Array.isArray(trips) && (
+        <ul>
+          {trips.map((trip) => (
+            <li key={trip.id} className="animate__animated animate__bounce">
+              <h2>
+                <i>{trip.title}</i>
+              </h2>
+              <p>
+                <i>{trip.price}</i>
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+      <div className="filters">
+        <button
+          className="animate__animated animate__rubberBand"
+          onClick={() => setUrl("http://localhost:3000/trips")}
+        >
+          All Trips
+        </button>
+        <button
+          className="animate__animated animate__rubberBand"
+          onClick={() => setUrl("http://localhost:3000/trips?location=USA")}
+        >
+          America Trips
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
     export default TripList
