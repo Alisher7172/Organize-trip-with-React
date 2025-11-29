4d7af5d3ce678cdf5bc653138988cd495218b52c
Your Name <alisher20021212@gmail.com>
2025-11-29 15:35:38 +0200

    added hook and trip sample

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,31 +1,45 @@
 // style 
 import  './TripList.css';
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useFetch,  } from '../hooks/useFetch';
 
 function TripList() {
-    const [trips, setTrips] = useState([]);
-    useEffect(() => {
-         fetch('http://localhost:3000/trips')
-        .then(response => response.json())
-        .then(data => setTrips(data));
-    }, []);
+    // const [trips, setTrips] = useState([]);
+    const [url, setUrl] = useState('http://localhost:3000/trips');
+    const { data: trips, isPending, error } = useFetch(url) ;
     console.log(trips);
+
+    // const fetchTrips = useCallback(async () => {
+    //     const response = await fetch(url);
+    //     const data = await response.json();
+    //     setTrips(data);
+    // }, [url]);
+    // useEffect(() => {
+    //     fetchTrips();
+    // }, [fetchTrips]);
+    
    
 
   return (
     <div className='trip-list'>
         <h1 className='main-txt animate__animated animate__flash'><i>Trip List</i></h1>
+        {isPending && <div className='loading animate__animated animate__heartBeat'><i>Loading Trips...</i></div>}
+        {error && <div className='error animate__animated animate__shakeX'><i>{error}</i></div>}
         <ul>
-            {trips.map(trip => (
+            {trips && trips.map(trip => (
                 <li key={trip.id} className='animate__animated animate__bounce'>
                     <h2><i>{trip.title}</i></h2>
                     <p><i>{trip.price}</i></p>
                 </li>
             ))}
         </ul>
+        <div className='filters'>
+            <button className='animate__animated animate__rubberBand' onClick={() => setUrl('http://localhost:3000/trips')}>All Trips</button>
+            <button className='animate__animated animate__rubberBand' onClick={() => setUrl('http://localhost:3000/trips?location=USA')}>America Trips</button>
+        </div>
     </div>
   )
 }
 
-export default TripList
+    export default TripList
